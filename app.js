d866569fd234577ff3a7270af6cf29b43d9f28b0
Brandon <30816431+Brandon00x@users.noreply.github.com>
2021-10-31 12:27:27 -0400

    Add support for relative volume and absolute volume.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const http = require("http");
 var nodeLIRC = require('node-lirc');
 nodeLIRC.init();
 
+//create server to listen
 const server = http.createServer((req, res) => {
     let data = '';
     if (req.url !== "/stero" || req.method !== "POST") {
@@ -13,14 +14,47 @@ const server = http.createServer((req, res) => {
         data += chunk;
       })
       req.on('end', () => {
-        console.log(JSON.parse(data))
+        console.log("Volume Relative: " + JSON.parse(data).volRelative);
+        console.log("Volume Absolute: " + JSON.parse(data).volAbsolute);
         res.end();
-        handleResponse()
+        determineVolType()
       })
-})
+  
+  function determineVolType() {
+    //math variables
+    let volPercent = JSON.parse(data).volRelative
+    let volAbsolute = JSON.parse(data).volAbsolute
+    let counterRelative = Math.abs(volPercent/10)
+    let counterAbsolute = Math.abs(volAbsolute/10)
 
-function handleResponse(){
-    nodeLIRC.send("Logitech_Z906", "VOLUME_DOWN");
-}
+    //use relative vol
+    if (volPercent != 0 && volPercent != undefined){
+      for (i = 0; i < counterRelative; i++){
+        nodeLIRC.send("Logitech_Z906", "VOLUME_DOWN");
+      }
+    }
+    //use vol absolute
+    else if (volAbsolute != 0 && volPercent != undefined){
+      for (i = 0; i < 12; i++){
+        nodeLIRC.send("Logitech_Z906", "VOLUME_DOWN");
+      }
+      //Google Assitant does not Support Volume Up Unless Configured With Device, I use a Pi. (using absolute volume and setting to 0 is a workaround.)
+      for (i = 0; i < counterAbsolute; i++){
+        nodeLIRC.send("Logitech_Z906", "VOLUME_UP");
+      }
+    }
+  }
+
+  //To Be Used In Future
+  function powerOff(){
+    nodeLIRC.send("Logitech_Z906", "POWER");
+  }
+  function inputOptical(){
+    nodeLIRC.send("Logitech_Z906", "INPUT_3");
+  }
+  function inputBluetooth(){
+    nodeLIRC.send("Logitech_Z906", "INPUT_7");
+  }     
+})
 
 server.listen(8081, () => console.log("Webhook Server Listening on 8081."))
